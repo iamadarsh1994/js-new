32b7d0186a53f34b1eb1efb098985c1e00a80f26
Adarsh Mishra <40768847+iamadarsh1994@users.noreply.github.com>
2023-12-17 21:21:28 +0000

    arrow fun done

---
+++ arrow.js
@@ -0,0 +1,26 @@
+
+// create an arrow function for multiplication of two no
+let multiply =(a, b) =>{
+    return a*b;
+  }
+ console.log(multiply(30, 40));
+ 
+ let sum =( x, y ) =>{
+   return x+y;
+ }
+ console.log(sum(30, 40));
+ 
+ let subs = (c, d) =>{
+   return c-d;
+ }
+ console.log(subs(30, 40));
+ 
+ let div = (e, f) =>{
+   return e/f;
+ }
+ console.log(div(1600, 40));
+ 
+ const greet =() =>{
+   console.log("hello world by adarsh");
+ }
+ greet();
